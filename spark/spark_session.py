ea1996f66591ffd9ee52fff05231ef09382daf40
Subhajoy Ghosh <ghoshsubhajoy02@gmail.com>
2026-05-13 23:07:03 +0530

    Limit Spark memory to 512m to prevent OOM in Docker

--- spark/spark_session.py
+++ spark/spark_session.py
@@ -4,17 +4,14 @@ def get_spark():
     return (
         SparkSession.builder
         .appName("BatchPipelineLevel1")
-        .config(
-            "spark.jars.packages",
-            "org.apache.hadoop:hadoop-aws:3.3.4"
-        )
-        .config(
-            "spark.hadoop.fs.s3a.impl",
-            "org.apache.hadoop.fs.s3a.S3AFileSystem"
-        )
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.4")
+        .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
         .config(
             "spark.hadoop.fs.s3a.aws.credentials.provider",
             "com.amazonaws.auth.DefaultAWSCredentialsProviderChain"
         )
+        .config("spark.driver.memory", "512m")
+        .config("spark.executor.memory", "512m")
+        .config("spark.sql.shuffle.partitions", "2")
         .getOrCreate()
     )
